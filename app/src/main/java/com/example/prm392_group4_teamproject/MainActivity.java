582da3127781aabd4a2363cf91eda0eb0a6f5482
Thanh Binh <binhpthe173173@fpt.edu.vn>
2025-06-11 11:04:41 +0700

    all setting profile before using app

--- app/src/main/java/com/example/prm392_group4_teamproject/MainActivity.java
+++ app/src/main/java/com/example/prm392_group4_teamproject/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.register_screen);
+        setContentView(R.layout.location_permission);
 
     }
 }
