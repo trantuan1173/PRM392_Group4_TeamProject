7c707e3d4d742d722bbd326ab855c2a55209a464
Tran Anh Tuan <trantuan1172003@gmail.com>
2025-07-10 16:46:45 +0700

    Update MainActivity.java

--- App/app/src/main/java/com/example/prm392_group4_teamproject/MainActivity.java
+++ App/app/src/main/java/com/example/prm392_group4_teamproject/MainActivity.java
@@ -1,79 +1,28 @@
 package com.example.prm392_group4_teamproject;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-import com.example.prm392_group4_teamproject.Cards.cards;
-import com.example.prm392_group4_teamproject.Cards.arrayAdapter;
-
-import com.yuyakaido.android.cardstackview.CardStackLayoutManager;
-import com.yuyakaido.android.cardstackview.CardStackListener;
-import com.yuyakaido.android.cardstackview.CardStackView;
-import com.yuyakaido.android.cardstackview.Direction;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.swipe_screen);
-
-        CardStackView cardStackView = findViewById(R.id.card_stack_view);
-        CardStackLayoutManager manager = new CardStackLayoutManager(this, new CardStackListener() {
-            @Override
-            public void onCardDragging(Direction direction, float ratio) {
-                // xử lý khi vuốt
-            }
-
-            @Override
-            public void onCardSwiped(Direction direction) {
-                // xử lý khi vuốt xong
-            }
-
-            @Override
-            public void onCardRewound() {
-                // xử lý khi card quay lại
-            }
-
-            @Override
-            public void onCardCanceled() {
-                // khi hủy vuốt (card trở về giữa)
-            }
-
-            @Override
-            public void onCardAppeared(View view, int position) {
-                // khi 1 card bắt đầu hiển thị
-            }
-
-            @Override
-            public void onCardDisappeared(View view, int position) {
-                // khi 1 card biến mất sau khi vuốt
-            }
-        });
-        cardStackView.setLayoutManager(manager);
-
-        List<cards> userList = new ArrayList<>();
-        userList.add(new cards("1", "Tuấn", "https://i.imgur.com/Xtq3M7G.jpeg"));
-        userList.add(new cards("2", "Trang", "https://i.imgur.com/fz4BMtW.jpeg"));
-        userList.add(new cards("3", "Minh", "https://i.imgur.com/K6fW3Ou.jpeg"));
-
-        arrayAdapter adapter = new arrayAdapter(userList, this);
-        cardStackView.setAdapter(adapter);
-        Button goToRegisterButton = findViewById(R.id.goToRegisterButton);
-        goToRegisterButton.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, RegistrationActivity.class);
+        SharedPreferences prefs = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+        String token = prefs.getString("auth_token", null);
+        Log.d("MainActivity", "Token: " + token);
+        if (token != null) {
+            Intent intent = new Intent(MainActivity.this, DashboardActivity.class);
             startActivity(intent);
-        });
+            finish();
+            return;
+        }
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.splash_screen);
     }
 }
